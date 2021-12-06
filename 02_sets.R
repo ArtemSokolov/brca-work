992cba631311e9790663e786ab0bca0674ecb5ec
Artem Sokolov <artem.sokolov@gmail.com>
2021-12-06 15:48:11 -0500

    Verification against existing data

--- 02_sets.R
+++ 02_sets.R
@@ -1,8 +1,16 @@
 library(tidyverse)
 
+vAll <- scan("data/all-genes.txt", what=character())
+core282 <- scan("sets/core282.txt", what=character())
+
 GS <- read_tsv("data/pub-sets.tsv", col_types=cols()) %>%
     select(-URL) %>%
     mutate(across(`Gene Set`, str_split, ", "),
-           across(PMID, partial(str_c, "PMID:"))) %>%
+           across(PMID, partial(str_c, "PMID"))) %>%
     with(set_names(`Gene Set`, PMID))
 
+nDiff <- map(GS, setdiff, vAll) %>% map_int(length)
+stopifnot(all(nDiff == 0))
+
+iwalk(GS, ~cat(.x, sep="\n",
+               file=str_c("sets/", .y, ".txt")))
