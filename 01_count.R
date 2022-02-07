47d387a8b66596ef7904e811266d1320bae20959
Artem Sokolov <artem.sokolov@gmail.com>
2022-02-07 15:22:11 -0500

    Updated representation of PMID counts

--- 01_count.R
+++ 01_count.R
@@ -15,19 +15,20 @@ PMID <- jsonlite::read_json('data/pmids_for_drugs.json') %>%
     mutate(Evidence = PubChem + MeSH + Grounding) %>%
     left_join(M, by="Drug")
 
-write_csv(PMID, 'data/pmids_for_drugs_tidy.csv')
+##write_csv(PMID, 'data/pmids_for_drugs_tidy.csv')
 
-plotCounts <- function() {
-    X <- PMID %>% group_by(Drug) %>%
+plotCounts <- function(.df, nThresh=100) {
+    X <- .df %>% group_by(Drug) %>%
         summarize(nPMID = length(PMID),
                   Class = unique(Class)) %>%
         arrange(Class, desc(nPMID)) %>%
         mutate(Drug = factor(Drug,Drug),
-               lbl = ifelse(nPMID >= 100, as.character(nPMID), ""),
+               lbl = ifelse(nPMID >= nThresh, as.character(nPMID), ""),
                lbl = str_c(lbl, "  "),
-               nPMID = pmin(nPMID,100))
+               nPMID = pmin(nPMID,nThresh))
 
-    pal <- c("white", ggthemes::few_pal()(8), "darkgray")
+    pal <- c("white", ggthemes::few_pal()(8), "darkgray") %>%
+        set_names(unique(PMID$Class))
     ggplot(X, aes(x=Drug, y=nPMID)) +
         theme_minimal() +
         geom_bar(stat='identity', color='gray', aes(fill=Class)) +
@@ -38,9 +39,13 @@ plotCounts <- function() {
         scale_fill_manual(values=pal) +
         xlab("Agent") +
         theme(panel.grid.major.x = element_blank(),
-              axis.text.x = element_text(angle=90, hjust=1, vjust=0.5)) +
-        ggsave("plots/pmid-count.png", width=10, height=4)
+              axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
 }
+
+PMID %>% plotCounts() %>%
+    ggsave("plots/01-pmid-count.png", ., width=10, height=4)
+PMID %>% filter(Evidence == 3) %>% plotCounts(50) %>%
+    ggsave("plots/01-pmid-count3.png", ., width=5, height=4)
 
 plotCitations <- function() {
     X <- PMID %>% filter(!is.na(nCite)) %>%
@@ -48,11 +53,11 @@ plotCitations <- function() {
         distinct() %>%
         mutate( Label = ifelse(nCite>500, PMID, "") )
 
-    ggplot(X, aes(x=Year, y=nCite)) +
+    gg1 <- ggplot(X, aes(x=Year, y=nCite)) +
         theme_minimal() + geom_point() +
         ggrepel::geom_text_repel(aes(label=Label), nudge_y=50) +
-        ylab("Number of citations") +
-        ggsave("plots/cite-count.png", width=8, height=6)
+        ylab("Number of citations")
+    ggsave("plots/01-cite-count.png", gg1, width=8, height=6)
 
     nTop <- c("2021"=0, "2020"=1, "2019"=1, "2018"=3, "2017"=3, "2016"=5,
               "2015"=2, "2014"=3, "2014"=3, "2013"=3, "2012"=6, "2011"=2, "2010"=3)
@@ -64,7 +69,7 @@ plotCitations <- function() {
                Label = ifelse(Rank <= nTop[as.character(Year)], PMID, ""),
                Highlight = ifelse(Label == "", "no", "yes"))
     
-    ggplot(X1, aes(x=nCite, y=Year, color=Highlight)) +
+    gg2 <- ggplot(X1, aes(x=nCite, y=Year, color=Highlight)) +
         theme_minimal() + geom_point() +
         ggrepel::geom_text_repel(aes(label=Label), size=3, nudge_y=-0.25) +
         scale_y_continuous(breaks=seq(2010,2021),
@@ -72,8 +77,12 @@ plotCitations <- function() {
         scale_x_continuous(breaks=seq(0,700,by=100),
                            labels=c(as.character(seq(0,600,by=100)), "700+"),
                            name="Number of citations") +
-        scale_color_manual(values=c("yes"="red", "no"="black"), guide=FALSE) +
+        scale_color_manual(values=c("yes"="red", "no"="black"), guide='none') +
         theme(panel.grid.major.y = element_blank(),
-              panel.grid.minor.y = element_blank()) +
-        ggsave("plots/cite-count-2010+.png", width=8, height=6)
+              panel.grid.minor.y = element_blank())
+
+    ggsave("plots/01-cite-count-2010+.png", gg2, width=8, height=6)    
 }
+
+plotCitations()
+
