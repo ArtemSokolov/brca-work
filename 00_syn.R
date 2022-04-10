e3d8c3728a54eaf6679ce81455b419b539a2d51b
Artem Sokolov <artem.sokolov@gmail.com>
2022-04-10 20:50:19 +0000

    Additional setup

--- 00_syn.R
+++ 00_syn.R
@@ -2,4 +2,4 @@ dl <- synExtra::synDownloader('/workspace/data')
 dl('syn26486688', 'syn26541985')
 
 dl_auc <- synExtra::synDownloader('/workspace/data/aucs')
-dl_auc('syn27221487''syn27221488', 'syn27221489')
+dl_auc('syn27221487', 'syn27221488', 'syn27221489')
