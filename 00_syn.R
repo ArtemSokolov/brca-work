63c2169d3969961572460334631021364a19f297
Artem Sokolov <artem.sokolov@gmail.com>
2022-04-10 22:14:01 +0000

    Using relative paths

--- 00_syn.R
+++ 00_syn.R
@@ -1,5 +1,5 @@
-dl <- synExtra::synDownloader('/workspace/data')
+dl <- synExtra::synDownloader('data')
 dl('syn26486688', 'syn26541985')
 
-dl_auc <- synExtra::synDownloader('/workspace/data/aucs')
+dl_auc <- synExtra::synDownloader('data/aucs')
 dl_auc('syn27221487', 'syn27221488', 'syn27221489')
