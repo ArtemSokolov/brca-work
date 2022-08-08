c22079b35b4b9b6f3ec8fa29b29a51acc0345c65
Artem Sokolov <artem.sokolov@gmail.com>
2022-08-08 21:22:21 +0000

    Consolidation of scripts

--- 01_count.R
+++ 01_count.R
@@ -88,3 +88,19 @@ plotCitations <- function() {
 }
 
 plotCitations()
+
+lit <- PMID %>% mutate(PMID = str_c("PMID:", PMID))
+
+# Count the number of unique drugs mentioned in each PMID
+unq_cnt <- group_by(lit, PMID) %>%
+    summarize(nDrug = length(unique(Drug)), .groups = "drop")
+
+# Top 50 papers for each drug
+res <- inner_join(lit, unq_cnt, by = "PMID") %>%
+    select(Drug, PMID, nDrug, Evidence, Year, nCite) %>%
+    group_by(Drug) %>%
+    arrange(nDrug, desc(Evidence), desc(Year), desc(nCite)) %>%
+    slice_head(n = 50) %>%
+    ungroup()
+
+write_csv(res, "output/lit-cand.csv")
