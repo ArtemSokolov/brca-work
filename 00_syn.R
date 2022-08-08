fb8b44a3802f7ffced06b0062b90aedb63fe491c
Artem Sokolov <artem.sokolov@gmail.com>
2022-08-08 21:14:43 +0000

    Adjusted data download

--- 00_syn.R
+++ 00_syn.R
@@ -1,5 +1,10 @@
 dl <- synExtra::synDownloader("data")
-dl("syn26486688", "syn26541985", "syn26560911", "syn26560927", "syn26486674")
+dl(
+ "syn26486688",     # agents_metadata.csv
+ "syn26541985",     # anova.csv
+ "syn26560911",     # lit-meta.tsv
+ "syn26560927"      # pmids_for_drugs_tidy.csv
+)
 
 dl_auc <- synExtra::synDownloader("data/aucs")
 dl_auc("syn27221487", "syn27221488", "syn27221489")
