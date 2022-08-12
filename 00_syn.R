58aed183c5c9d16f06b47783d8cc4573b0bc3334
Artem Sokolov <artem.sokolov@gmail.com>
2022-08-12 14:06:24 +0000

    Consolidated all signatures

--- 00_syn.R
+++ 00_syn.R
@@ -2,15 +2,8 @@ dl <- synExtra::synDownloader("data")
 dl(
  "syn26486688",     # agents_metadata.csv
  "syn26541985",     # anova.csv
- "syn26560911",     # lit-meta.tsv
  "syn26560927"      # pmids_for_drugs_tidy.csv
 )
 
 dl_auc <- synExtra::synDownloader("data/aucs")
 dl_auc("syn27221487", "syn27221488", "syn27221489")
-
-dl_sig1 <- synExtra::synDownloader("data/sigs/lit")
-dl_sig1(synExtra::synGlob("syn26529258", "*.txt"))
-
-dl_sig2 <- synExtra::synDownloader("data/sigs/univ")
-dl_sig2(synExtra::synGlob("syn26529816", "*.txt"))
--- txt2yml.R
+++ txt2yml.R
@@ -11,17 +11,21 @@ vfn <- set_names(vfn, str_sub(basename(vfn), 1, -5))
 # Load the signatures
 sigs <- vfn %>%
     map(scan, what = character()) %>%
-    map(~list(Signature = .x))
+    map(~list(signature = .x))
 
 # Load all relevant metadata
-metad <- read_csv("data/agents_metadata.csv")
-metal <- read_tsv("data/lit-meta.tsv") %>%
-    select(-Title, -URL)
+meta <- read_tsv("data/lit-meta.tsv") %>%
+    select(-Title, -URL, -Size) %>%
+    rename_with(str_to_lower, -PMID) %>%
+    rename_with(~str_replace_all(.x, " ", "-")) %>%
+    mutate(across(PMID, ~str_c("PMID", .x))) %>%
+    mutate(across(type, recode, Universal = "Prior-based"))
 
 # Combine signatures with metadata
-res <- metal %>%
-    mutate(across(PMID, ~str_c("PMID", .x))) %>%
+res <- meta %>%
     split(., .$PMID) %>%
     map(select, -PMID) %>%
     map(as.list) %>%
     list_merge(!!!sigs)
+
+yaml::write_yaml(res, "literature.yml")
